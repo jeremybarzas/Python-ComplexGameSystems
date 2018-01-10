7e0f09a4ee30c0091b4c30963f86230ef43c2156
Jeremy Barzas <jbarzas@gmail.com>
2018-01-10 16:40:10 -0600

    set up methods variables and coments

--- OperatorParser.py
+++ OperatorParser.py
@@ -1,20 +1,33 @@
-class parser:
+class Parser:
+    ## Fields
+    # constructor
     def __init__(self):
         self.expression = None
         self.varaibles = None
         self.clauses = None
         self.pairs = None
     
+    ## Methods
+    # set expression to be parsed
     def set_expression(self, string)
         self.expression = string
         return
 
-    def get_variables(self, parameter_list):
-        return
-
-    def get_clauses(self, parameter_list):
-        return
-
-    def get_pairs(self, parameter_list):
+    # calculate and assign varaibles field
+    def get_variables(self):
+        # parse expression adding the characters into variables while omiting operators, symbols and, duplicates
+        
         return
     
+    # calculate and assign clauses
+    def get_clauses(self):
+        # parse expression adding strings into clauses determined by () and operators
+
+        return
+
+    # calcualte and assign pairs
+    def get_pairs(self, input):
+        # parse input adding a tuple of each input character + each variable 
+        
+        return
+   
