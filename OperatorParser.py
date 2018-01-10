d4ed0b5b0565f773410093a96694b92f3fbfdf8a
Jeremy Barzas <jbarzas@gmail.com>
2018-01-10 15:46:58 -0600

    initial commit

---
+++ OperatorParser.py
@@ -0,0 +1,15 @@
+class parser:
+    def __init__(self):
+        self.expression = None
+        self.varaibles = None
+        self.clauses = None
+        self.pairs = None
+    
+    def get_variables(self, parameter_list):
+        pass
+
+    def get_clauses(self, parameter_list):
+        pass
+
+    def get_pairs(self, parameter_list):
+        pass
