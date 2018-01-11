5e8258e456534eb39754a98820ee37ad6bdafb29
Jeremy Barzas <jbarzas@gmail.com>
2018-01-10 18:02:41 -0600

    end of day commit

--- OperatorParser.py
+++ OperatorParser.py
@@ -2,10 +2,10 @@ class Parser:
     ## Fields
     # constructor
     def __init__(self):
-        self.expression = None
-        self.varaibles = None
-        self.clauses = None
-        self.pairs = None
+        self.expression = ""
+        self.variables = []
+        self.clauses = []
+        self.pairs = []
     
     ## Methods
     # set expression to be parsed
@@ -13,10 +13,20 @@ class Parser:
         self.expression = string
         return
 
-    # calculate and assign varaibles field
+    # calculate and assign variables field
     def get_variables(self):
-        # parse expression adding the characters into variables while omiting operators, symbols and, duplicates
-        
+        # parse expression adding the characters into variables while omiting operators, symbols and, duplicates        
+        for char in expression:
+            # check for symbol
+            if((char > 64 and char < 90) or (char > 97 and char > 122))
+                variables.append()
+
+        # check for and remove duplicate variables
+        for var in variables:
+            for c in variables:
+                if (variables.index(var) != variables.index(c) and var == c)
+                    variables.remove(var)
+
         return
     
     # calculate and assign clauses
@@ -27,7 +37,16 @@ class Parser:
 
     # calcualte and assign pairs
     def get_pairs(self, input):
-        # parse input adding a tuple of each input character + each variable 
+        # parse input adding a tuple of each input character + each variable into pairs
         
         return
    
+
+def main():
+    parser = Parser()
+    formula = "(a + b) * (c + a + b)"
+    parser.set_expression(formula)
+    parser.get_variables()
+
+    print parser.variables
+
