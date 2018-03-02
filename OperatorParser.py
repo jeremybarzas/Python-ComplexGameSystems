cb7bc9ced83692b22fbae63c5e52dbb773940673
Jeremy Barzas <jbarzas@gmail.com>
2018-03-02 12:13:23 -0600

    get clauses and get pairs working

--- OperatorParser.py
+++ OperatorParser.py
@@ -28,24 +28,61 @@ class Parser:
     
     # calculate and assign clauses
     def get_clauses(self):
-        # parse expression adding strings into clauses determined by () and operators
-
+        # parse expression adding strings into clauses determined by () and operators { "(" = 40, ")" = 41 }        
+        clause = ""
+        clausefound = False
+        for char in self.expression:
+            c = ord(char)                
+            if(c == 40):                    
+                clausefound = True
+            if(clausefound):
+                clause += char
+            if(c == 41):
+                clausefound = False
+                self.clauses.append(clause)
+                clause = ""
         return
 
     # calcualte and assign pairs
-    def get_pairs(self, input):
+    def get_pairs(self):
         # parse input adding a tuple of each input character + each variable into pairs
-        
+        newvars = []
+        variable = ''
+        for char in self.expression:
+            c = ord(char)
+            if(c == 33):
+                variable = char
+                continue
+            if(c >= 97 and c <= 122) or (c >= 65 and c <= 90):
+                if(variable == ''):
+                    if(newvars.__contains__(char) == False):
+                        # add to variables
+                        variable += char
+                        newvars.append(variable)
+                        variable = ''
+                else:
+                    # add to variables
+                    variable += char
+                    newvars.append(variable)
+                    variable = ''
+
+        for var in newvars:
+            tup = (var, '')
+            self.pairs.append(tup)          
         return
 
 def main():
     parser = Parser()
-    formula = '(a + b) * (C + a + B)'
+    formula = '(a) * (b) * (!c) * (!d)'
     parser.set_expression(formula)
     parser.get_variables()
+    parser.get_clauses()
+    parser.get_pairs()
 
     print parser.expression
     print parser.variables
+    print parser.clauses
+    print parser.pairs
     
 if __name__ == "__main__":
     main()
